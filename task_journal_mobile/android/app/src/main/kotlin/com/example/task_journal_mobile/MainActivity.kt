3055d59cf3586e3bf09df17aad2e93abd0b49599
JovanZdravkovic <jovan.zdravkovic882@gmail.com>
2025-03-29 00:04:58 +0100

    Added project skeleton and colors file

---
+++ task_journal_mobile/android/app/src/main/kotlin/com/example/task_journal_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.task_journal_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ task_journal_mobile/lib/utils/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const primary = Color(0xff23C686);
+const primaryLight = Color(0xff2DEDA4);
+const primaryDark = Color(0xff1CA670);
+const secondary = Color(0xffFC9D45);
+const secondaryDark = Color(0xffF28B2C);
+const dark = Color(0xff1D3853);
+const darkLight = Color(0xff7A9295);
+const grayLight = Color(0xffFAFAFA);
+const grayDark = Color(0xffF5F5F5);
+const danger = Color(0xffF32013);
+const starYellow = Color(0xffFFEA00);
